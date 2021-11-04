61a68fbd3df9ea11949844fa4e0db24187686111
Tom Szymczuk <tszymczuk@student.bridgew.edu>
2021-11-04 10:33:45 -0400

    Getting HTTP

--- src/bsu/comp152/Main.java
+++ src/bsu/comp152/Main.java
@@ -1,15 +1,33 @@
 package bsu.comp152;
 
+import java.io.IOException;
 import java.net.URI;
 import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
 
 public class Main {
 
     public static void main(String[] args) {
         var dataGrabber = HttpClient.newHttpClient();
         var requestBuilder = HttpRequest.newBuilder();
-        var dataRequest = requestBuilder.uri(URI.create("http://universities.hipolabs.com/search?name=Young")).build();
+        var webRequest = requestBuilder.uri(URI.create("http://universities.hipolabs.com/search?name=Young")).build();
 
+        HttpResponse<String> response = null;
+        try {
+            response = dataGrabber.send(webRequest, HttpResponse.BodyHandlers.ofString());
+        }
+        catch (IOException exceptionObject){
+            System.out.println("Error connecting to network or site");
+        }
+        catch (InterruptedException e){
+            System.out.println("Connection to site broken");
+        }
+        if(response == null){
+            System.out.println("Something went wrong getting data from the network");
+            System.exit(-1);
+        }
+        var usefulData = response.body();
+        System.out.println(usefulData);
     }
 }
